30c946c2617ddcbf3c9ff999829b16d6959a8052
arielmbomakaeyros <ariel.mboma@kaeyros-analytics.com>
2024-03-26 16:33:42 +0100

    better form UI

--- src/app/components/form/SignUpForm.tsx
+++ src/app/components/form/SignUpForm.tsx
@@ -5,6 +5,9 @@ import { ClientRegistrationFormValues } from './signupForm.td';
 import { useRouter } from 'next/navigation';
 import ThreeDotsLoadingAnimation from '@/components/loaders/TreeDotsLoading';
 import toast from 'react-hot-toast';
+import { Checkbox } from '@/components/ui/checkbox';
+import { Calendar } from '@/components/ui/calendar';
+import { DatePickerDemo } from '@/components/ui/DatePicker';
 
 const SignUpForm = () => {
 
@@ -24,14 +27,14 @@ const SignUpForm = () => {
             // next: { revalidate: 10 }, 
         }); 
   
-        console.log("-----=-------====-----------====", users)
+        // console.log("-----=-------====-----------====", users)
   
         if (!users.ok) {
             throw new Error("Failed to fetch users"); 
         } else {
             const test = await users.json()
             setEstablishedUser (test)
-            console.log("__________===", test)
+            // console.log("__________===", test)
             // return await users.json(); 
         }
     } catch (error) {
@@ -115,6 +118,15 @@ const SignUpForm = () => {
     setSelectedLevel ({ name: isExpert.name, status: true}); 
   }
 
+  const [ oswdMenber, setoswdMenber ] = useState ({
+    name: "OSWD Member", 
+    status: false , 
+  }); 
+
+  const handleOSWMemberSelection = () => {
+    setoswdMenber ({ name: oswdMenber.name, status: true})
+  }
+
   // console.log("selectedLevelselectedLevel", selectedLevel)
 
   const {
@@ -131,6 +143,10 @@ const SignUpForm = () => {
     // reset ()
     // return
     setIsLoading (true)
+    data = {
+        ...data, 
+        rLanguageLevel: selectedLevel?.name
+    }
     try {
       const result = await fetch("/api/users/signup", {
           method: "POST", 
@@ -157,21 +173,22 @@ const SignUpForm = () => {
     }
     reset ();
   }
+  const [date, setDate] = React.useState<Date | undefined>(new Date())
   return (
-    <form className='flex flex-col pt-[20rem] p-[1rem] sm:pt-[2rem] justify-center gap-3 overflow-x-hidden overflow-y-auto h-[600px] w-[100%] sm:h-auto' onSubmit={handleSubmit(onSubmit)}>
+    <form className='flex flex-col p-[1rem] sm:pt-[2rem] justify-center gap-3 overflow-x-hidden overflow-y-auto w-[100%] sm:h-auto' onSubmit={handleSubmit(onSubmit)}>
 
       {/* +++++++++ FIRSTNAME LASTNAME +++++++++++ */}
 
-      <div className='sm:grid sm:grid-cols-[repeat(2,50%)] sm:gap-3'>
+      <div className='sm:grid sm:grid-cols-[repeat(2,1fr)] sm:gap-3'>
         <div>
-            <label htmlFor="" className='text-black'>First Name</label>
+            <label htmlFor="" className='text-gray-500'>First Name</label>
             <input 
                 {...register("name", {
                     required: "Prenom Obligatoire", 
                 } )}
                 type='text' 
                 placeholder='Entrer votre Prenom ici'
-                className='py-4 px-4 rounded-2xl text-black border-[1px] border-gray outline-none w-full'
+                className='py-3 px-4 rounded-2xl text-black border-[1px] border-gray outline-none w-full'
             />
             <p className='text-[.9rem]' style={{
                 color: "red"
@@ -181,14 +198,14 @@ const SignUpForm = () => {
         </div>
 
         <div>
-            <label htmlFor="" className='text-black'>Last Name</label>
+            <label htmlFor="" className='text-gray-500'>Last Name</label>
             <input 
                 {...register("surname", {
                     required: "Nom Obligatoire", 
                 } )}
                 type='text' 
                 placeholder='Entrer votre Nom ici'
-                className='py-4 px-4 rounded-2xl text-black border-[1px] border-gray outline-none w-full'
+                className='py-3 px-4 rounded-2xl text-black border-[1px] border-gray outline-none w-full'
             />
             <p className='text-[.9rem]' style={{
                 color: "red"
@@ -203,9 +220,9 @@ const SignUpForm = () => {
 
       {/* +++++++++ PHONE EMAIL +++++++++++ */}
 
-      <div className='sm:grid sm:grid-cols-[repeat(2,50%)] sm:gap-3'>
+      <div className='sm:grid sm:grid-cols-[repeat(2,1fr)] sm:gap-3'>
           <div>
-              <label htmlFor="" className='text-black'>Phone Number</label>
+              <label htmlFor="" className='text-gray-500'>Phone Number</label>
               <input 
                   {...register("phoneNumber", {
                       required: "Contact est obligatoire", 
@@ -216,7 +233,7 @@ const SignUpForm = () => {
                   } )}
                   type='number' 
                   placeholder='Entrez votre contact ici'
-                  className='py-4 px-4 rounded-2xl text-black border-[1px] border-gray outline-none w-full'
+                  className='py-3 px-4 rounded-2xl text-black border-[1px] border-gray outline-none w-full'
               />
               <p className='text-[.9rem]' style={{
                   color: "red"
@@ -225,14 +242,14 @@ const SignUpForm = () => {
               </p>
           </div>
           <div>
-              <label htmlFor="" className='text-black'>Email</label>
+              <label htmlFor="" className='text-gray-500'>Email</label>
               <input 
                   {...register("email", {
                       required: "Email Obligatoire", 
                   } )}
                   type='text' 
                   placeholder='Entrex votre addresse email ici'
-                  className='py-4 px-4 rounded-2xl text-black border-[1px] border-gray outline-none w-full'
+                  className='py-3 px-4 rounded-2xl text-black border-[1px] border-gray outline-none w-full'
               />
               {/* <p className='text-[.9rem]' style={{
                   color: "red"
@@ -246,25 +263,32 @@ const SignUpForm = () => {
 
       {/* +++++++++ GENDER DATEOFBIRTH +++++++++++ */}
 
-      <div className='sm:grid sm:grid-cols-[repeat(2,50%)] sm:gap-3'>
+      <div className='sm:grid sm:grid-cols-[repeat(2,1fr)] sm:gap-3'>
           <div>
-              <label htmlFor="" className='text-black'>Sex</label>
+              <label htmlFor="" className='text-gray-500'>Sex</label>
               <input 
                   {...register("gender" )}
                   type='text' 
                   placeholder='Entrez votre Gender'
-                  className='py-4 px-4 rounded-2xl text-black border-[1px] border-gray outline-none w-full'
+                  className='py-3 px-4 rounded-2xl text-black border-[1px] border-gray outline-none w-full'
               />
           </div>
           <div>
-              <label htmlFor="" className='text-black'>Date of Birth</label>
+              <label htmlFor="" className='text-gray-500'>Date of Birth</label>
+              {/* <Calendar
+                    mode="single"
+                    selected={date}
+                    onSelect={setDate}
+                    className="rounded-md border"
+                /> */}
+            {/* <DatePickerDemo /> */}
               <input 
                   {...register("dateOfBirth", {
                       // required: "Entrez nom de votre compagnie", 
                   })}
                   type='date' 
                   placeholder='Entrez votre date de naissance'
-                  className='py-4 px-4 rounded-2xl text-black border-[1px] border-gray outline-none w-full'
+                  className='py-3 px-4 rounded-2xl text-black border-[1px] border-gray outline-none w-full'
               />
           </div>
       </div>
@@ -272,7 +296,7 @@ const SignUpForm = () => {
       {/* +++++++++ GENDER DATEOFBIRTH +++++++++++ */}
 
       <div className='sm:grid sm:grid-cols-[repeat(5,1fr)] sm:gap-3'>
-          <div onChange={ () => handleStudentSelection () } className={ "flex flex-row justify-around gap-2 bg-white py-[.5rem] px-[1rem] rounded-3xl cursor-pointer" }>
+          <div onChange={ () => handleStudentSelection () } className={ "flex flex-row justify-around bg-white py-[.5rem] px-[1rem] rounded-3xl cursor-pointer" }>
             <input 
                 // {...register("company_name", {
                 //     required: "Entrez nom de votre compagnie", 
@@ -280,7 +304,7 @@ const SignUpForm = () => {
                 name='student'
                 type='checkbox' 
                 // onChange={ () => handleSemaSelection () }
-                onChange={ () => handleExpertSelection() }
+                // onChange={ () => handleStudentSelection() }
                 checked={ isStudent.status }
                 className='cursor-pointer rounded-2xl text-black border-[1px] border-gray outline-none w-full'
             />
@@ -294,26 +318,26 @@ const SignUpForm = () => {
         (
         <div className='sm:grid sm:grid-cols-[repeat(2,1fr)] sm:gap-3'>
           <div>
-              <label htmlFor="" className='text-black'>Faculty</label>
+              <label htmlFor="" className='text-gray-500'>Faculty</label>
               <input 
                   {...register("faculty", {
                       // required: "Entrez nom de votre compagnie", 
                   })}
                   type='text' 
                   placeholder='Enter your faculty Name'
-                  className='py-4 px-4 rounded-2xl text-black border-[1px] border-gray outline-none w-full'
+                  className='py-3 px-4 rounded-2xl text-black border-[1px] border-gray outline-none w-full'
               />
           </div>
 
           <div>
-              <label htmlFor="" className='text-black'>Research Domain</label>
+              <label htmlFor="" className='text-gray-500'>Research Domain</label>
               <input 
                   {...register("researchDomain", {
                       // required: "Entrez nom de votre compagnie", 
                   })}
                   type='text' 
                   placeholder='Enter your Research domain'
-                  className='py-4 px-4 rounded-2xl text-black border-[1px] border-gray outline-none w-full'
+                  className='py-3 px-4 rounded-2xl text-black border-[1px] border-gray outline-none w-full'
               />
           </div>
 
@@ -324,16 +348,16 @@ const SignUpForm = () => {
       {
         !isStudent?.status && 
         (
-          <div className=''>
+          <div className='sm:grid sm:grid-cols-[repeat(1,1fr)] sm:gap-3'>
             <div>
-                <label htmlFor="" className='text-black'>Profession</label>
+                <label htmlFor="" className='text-gray-500'>Profession</label>
                 <input 
                     {...register("profession", {
                         // required: "Entrez nom de votre compagnie", 
                     })}
                     type='text' 
                     placeholder='Enter your Profession'
-                    className='py-4 px-4 rounded-2xl text-black border-[1px] border-gray outline-none w-full'
+                    className='py-3 px-4 rounded-2xl text-black border-[1px] border-gray outline-none w-full'
                 />
             </div>
           </div>
@@ -364,9 +388,11 @@ const SignUpForm = () => {
                 />
                 <label htmlFor="" className='cursor-pointer text-black'>Low</label>
             </div>
-            
+
         </div>
+        R Language Level:
         <div className='sm:grid sm:grid-cols-[repeat(4,minmax(100px,_1fr))] flex flex-row gap-1 justify-start sm:gap-2  w-[60%]'>
+            {/* space-y-1 leading-none */}
             <div onClick={ () => handleHighSelection() } className={ "flex flex-row justify-center gap-2 bg-white py-[.5rem] px-[1rem] rounded-3xl cursor-pointer" }>
                 <input 
                     name='isHigh'
@@ -390,10 +416,30 @@ const SignUpForm = () => {
         </div>
       </div>
 
+      Are you a member of OWSD?
+      <div className='sm:grid sm:grid-cols-[repeat(4,1fr)] sm:gap-3'>
+          <div onChange={ () => handleOSWMemberSelection () } className={ "flex flex-row justify-around gap-2 bg-white py-[.5rem] px-[1rem] rounded-3xl cursor-pointer" }>
+            <input 
+                // {...register("company_name", {
+                //     required: "Entrez nom de votre compagnie", 
+                // })}
+                name='oswdMenber'
+                type='checkbox' 
+                // onChange={ () => handleSemaSelection () }
+                // onChange={ () => handleOSWMemberSelection() }
+                checked={ oswdMenber.status }
+                className='cursor-pointer rounded-2xl text-black border-[1px] border-gray outline-none w-[20px]'
+            />
+            {/* <Checkbox /> */}
+            <label htmlFor="" className='cursor-pointer text-black'>OSWD Member?</label>
+          </div>
+
+      </div>
+
       <div className="mt-4 w-full">
           <button
               // disabled={ isValid || isLoading ? false : true}
-              className={` ${ isValid ? "bg-backgroundOSWD text-white" : "bg-slate-500 text-white" } w-[20%]  py-4 px-4 rounded-2xl hover:bg-slate-950`}
+              className={` ${ isValid ? "bg-backgroundOSWD text-white hover:bg-slate-950" : "bg-slate-300 text-white" } border border-white w-[100px]  py-4 px-4 rounded-2xl `}
           >
               {isLoading ? <ThreeDotsLoadingAnimation color="white" /> : <>Save</>}
           </button>
--- src/app/module/SignupModule.tsx
+++ src/app/module/SignupModule.tsx
@@ -5,12 +5,14 @@ import Parteners from '../components/Parteners'
 const SignupModule = () => {
 
   return (
-    <div className='relative bg-gray-300 h-screen flex flex-col items-center justify-center'>
-      <h1 className='text-bold text-[1rem] sm:text-[2rem] mb-[1rem] absolute top-0 bg-white w-full flex justify-center'>Welcome to OSWD BootCamp Signup Form</h1>
-      <div className='flex flex-col gap-20 mt-[2rem] w-[100%] sm:w-[80%] lg:w-[60%] '>
+    <div className='relative bg-gray-100 h-[100%] flex flex-col items-center justify-center'>
+      <h1 className='text-center text-bold text-[1rem] sm:text-[2rem] mb-[1rem] sticky top-0 bg-white text-black w-full flex justify-center'>Welcome to OSWD BootCamp Signup Form</h1>
+      <div className='flex flex-col border border-white rounded-3xl justify-center gap-20 mt-[2rem] w-[100%] sm:w-[80%] lg:w-[50%] bg-slate-100'>
         <SignUpForm />
       </div>
-      <Parteners /> 
+      <div className='sticky flex justify-center bottom-0 lg:relative w-[100%] bg-gray-100 lg:bg-transparent'>
+        <Parteners /> 
+      </div>
     </div>
   )
 }
